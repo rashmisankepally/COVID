b72304772b6eefc3749bbaf2fb8377d9f8740f32
Rashmi Sankepally <rashmi.sankepally@gmail.com>
2020-04-28 21:20:03 +0000

    totally revamped writing to pubsub

--- twitter_to_pubsub/twitter_to_pubsub.py
+++ twitter_to_pubsub/twitter_to_pubsub.py
@@ -1,23 +1,34 @@
 """This script uses the Twitter Streaming API, via the tweepy library,
 to pull in tweets and publish them to a PubSub topic.
 """
-import base64
 import datetime
 import utils
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
+from google.cloud import pubsub_v1
 
-def publish(client, pubsub_topic, data_lines):
-    """Publish to the given pubsub topic."""
-    messages = []
-    for line in data_lines:
-        pub = line
-        messages.append({'data': pub})
-    body = {'messages': messages}
-    resp = client.projects().topics().publish(
-        topic=pubsub_topic, body=body).execute(num_retries=3)
-    return resp
+# Configure the connection
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path('bigdata-273220', 'got_COVID_tweets')
+
+# Function to write data to
+def write_to_pubsub(data):
+    try:
+        if data["lang"] == "en":
+
+            # publish to the topic, don't forget to encode everything at utf8!
+            publisher.publish(topic_path, data=json.dumps({
+                "text": data["text"],
+                "user_id": data["user_id"],
+                "id": data["id"],
+                "posted_at": datetime.datetime.fromtimestamp(data["created_at"]).strftime('%Y-%m-%d %H:%M:%S')
+            }).encode("utf-8"), tweet_id=str(data["id"]).encode("utf-8"))
+
+    except Exception as e:
+        print(e)
+        raise
+
 
 class StdOutListener(StreamListener):
     """A listener handles tweets that are received from the stream.
@@ -30,22 +41,22 @@ class StdOutListener(StreamListener):
     total_tweets = 1000000
     client = utils.create_pubsub_client(utils.get_credentials())
 
-    def write_to_pubsub(self, tw):
-        publish(self.client, 'projects/bigdata-273220/topics/got_COVID_tweets', tw)
-
+    def on_status(self, data):
+        write_to_pubsub(reformat_tweet(data._json))
+        return True
+    '''
     def on_data(self, data):
         """What to do when tweet data is received."""
         self.tweets.append(data)
         if len(self.tweets) >= self.batch_size:
-            self.write_to_pubsub(self.tweets)
+            write_to_pubsub(reformat_tweet(data._json))
             self.tweets = []
-        self.count += 1
-        # if we've grabbed more than total_tweets tweets, exit the script.
         if self.count > self.total_tweets:
             return False
         if (self.count % 1000) == 0:
             print('count is: %s at %s' % (self.count, datetime.datetime.now()))
         return True
+    '''
 
     def on_error(self, status):
         print(status)
@@ -54,6 +65,7 @@ if __name__ == '__main__':
     listener = StdOutListener()
     auth = OAuthHandler('MuDx4Cuoq2W5pr463fi1vivf1', 'mU5Vlmpu63vNPHYfm5AZOe7FlGB8RHEuwsxHUmtOb2I71h1O8J')
     auth.set_access_token('165781656-CRCOOZ6UWZD0rYNNwRKcTZKo5ztTd0a7MEeReGzg', 'aklRMf4da3A0OIYZjpPuwUBZzzQewY3yp5GKylx3mMarD')
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=False)
     stream = Stream(auth, listener)
     stream.filter(track=['coronavirus', 'COVID', 'COVID19', 'COVID-19', 'COVID_19'], is_async=True)
 
