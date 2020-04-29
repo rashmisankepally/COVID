3c4ed2f057ee8f08c7d84467eb639b2ca7bfd467
Rashmi Sankepally <rashmi.sankepally@gmail.com>
2020-04-29 01:15:25 +0000

    added format_tweet

--- twitter_to_pubsub/twitter_to_pubsub.py
+++ twitter_to_pubsub/twitter_to_pubsub.py
@@ -29,6 +29,44 @@ def write_to_pubsub(data):
         print(e)
         raise
 
+# Method to format a tweet from tweepy
+def reformat_tweet(tweet):
+    x = tweet
+
+    processed_doc = {
+        "id": x["id"],
+        "lang": x["lang"],
+        "retweeted_id": x["retweeted_status"]["id"] if "retweeted_status" in x else None,
+        "favorite_count": x["favorite_count"] if "favorite_count" in x else 0,
+        "retweet_count": x["retweet_count"] if "retweet_count" in x else 0,
+        "coordinates_latitude": x["coordinates"]["coordinates"][0] if x["coordinates"] else 0,
+        "coordinates_longitude": x["coordinates"]["coordinates"][0] if x["coordinates"] else 0,
+        "place": x["place"]["country_code"] if x["place"] else None,
+        "user_id": x["user"]["id"],
+        "created_at": time.mktime(time.strptime(x["created_at"], "%a %b %d %H:%M:%S +0000 %Y"))
+    }
+
+    if x["entities"]["hashtags"]:
+        processed_doc["hashtags"] = [{"text": y["text"], "startindex": y["indices"][0]} for y in
+                                     x["entities"]["hashtags"]]
+    else:
+        processed_doc["hashtags"] = []
+
+    if x["entities"]["user_mentions"]:
+        processed_doc["usermentions"] = [{"screen_name": y["screen_name"], "startindex": y["indices"][0]} for y in
+                                         x["entities"]["user_mentions"]]
+    else:
+        processed_doc["usermentions"] = []
+
+    if "extended_tweet" in x:
+        processed_doc["text"] = x["extended_tweet"]["full_text"]
+    elif "full_text" in x:
+        processed_doc["text"] = x["full_text"]
+    else:
+        processed_doc["text"] = x["text"]
+
+    return processed_doc
+
 
 class StdOutListener(StreamListener):
     """A listener handles tweets that are received from the stream.
